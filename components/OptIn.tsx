aa2fb8dd860211583f13d484e7c4f0ae73f899b2
abdur-rafi <abdurrafi403@gmail.com>
2023-08-14 08:28:43 +0600

    opt in pie chart done

--- components/OptIn.tsx
+++ components/OptIn.tsx
@@ -1,60 +1,115 @@
 import { SelectChangeEvent } from "@mui/material/Select";
 import { Dayjs } from "dayjs";
-import { useState } from "react";
+import { CSSProperties, useState } from "react";
 import { Cell, Legend, Pie, PieChart, Tooltip } from "recharts";
 import MyCard from "./card";
 import { GET_OPT_OUTS } from "../graphql/operations";
-import { useQuery } from "@apollo/client";
+import { useLazyQuery, useQuery } from "@apollo/client";
 import { MyDatePicker } from "./DatePicker";
 import { MealTimeDropDown } from "./MealTimeDropDown";
+import { Typography } from "@mui/material";
 
 
 export function OptInPieChart(){
     const [mData, setmData] = useState<any[]>([]);
     const [date, setDate] = useState<Dayjs | null>(null);
     const [mealTime, setMealTime] = useState("DINNER");
-    const options = ['DINNER', "LUNCH"]
-    const handleOptionChange = (e: SelectChangeEvent) => {
-        setMealTime(e.target.value);
-    }
-    const handleDate = (newValue: Dayjs | null) => {
-        setDate(newValue);
-    }
-
-    let { loading } = useQuery(
-        GET_OPT_OUTS,
-        {
-            variables: {
-                date: date == undefined ? new Date().toString() : date.toString(),
+    function getData(date : string, mealTime : string){
+        query({
+            variables : {
+                date : date,
                 mealTime : mealTime
-            },
-            onCompleted: (data) => {
+            },onCompleted: (data) => {
                 let arr =  []
                 arr.push({
-                    count : data.optedOutStats.total - data.optedOutStats.optedOut,
-                    name : "OptedIn"
+                    count : Math.round((data.optedOutStats.total - data.optedOutStats.optedOut) * 100. /data.optedOutStats.total),
+                    name : "%OptedIn"
                 })
                 arr.push({
-                    count : data.optedOutStats.optedOut,
-                    name : "OptedOut"
+                    count :  Math.round((data.optedOutStats.optedOut * 100.) / data.optedOutStats.total),
+                    name : "%OptedOut"
                 })
                 setmData(arr);
                 
             }
+        })
+    }
+    const setMealTime_ = (s : string) => {
+        setMealTime(s);
+        if(date){
+            getData(date.toString(), s);
         }
+    }
+    const handleDate = (newValue: Dayjs | null) => {
+        setDate(newValue);
+        if(newValue){
+            getData(newValue.toString(), mealTime);
+        }
+    }
+
+    let [ query, {data, loading} ] = useLazyQuery(
+        GET_OPT_OUTS,
     )
 
+    let textDivStyle : CSSProperties = {
+        padding : "10px 30px",
+        backgroundColor : "black",
+        borderRadius : 10,
+        margin : 10
+    }
 
+    console.log(data);
 
     return (
 
         <MyCard
-            title={<div>
-                 <MyDatePicker date={date} handleDate={handleDate} />
-                 <MealTimeDropDown setVal={setMealTime} val={mealTime} />
-            </div>}
+            title={
+                <div style={{
+                    display : "flex",
+                    justifyContent : "space-between",
+                    alignItems : "center"
+                }}>
+                    <h3>
+                        Opted Out
+                    </h3>
+                    <div style={{
+                        display : "flex",
+                        justifyContent : "flex-end",
+                        alignItems : "center"
+                    }}>
+                        <div style={{
+                            padding : "10px"
+                        }}>
+                            <MealTimeDropDown setVal={setMealTime_} val={mealTime} />
+                        </div>
+                        <MyDatePicker date={date} handleDate={handleDate} />
+                    </div>
+                </div>
+            }
             content={
-                <PieChart_ colors={['#FFE605', '#FFFFFF']} data={mData} dataKey="count" h={400} w={400} />
+                <div style={{
+                    display : "flex",
+                    alignItems : "center"
+                }}>
+                    <PieChart_ colors={['#FFE605', '#FFFFFF']} data={mData} dataKey="count" h={400} w={400} />
+                    <div>
+                        {
+                            data &&
+                            <div  style={textDivStyle}>
+                                <Typography variant="subtitle2"> Total Opted In </Typography>
+                                <Typography  variant="h5"> { data?.optedOutStats.total - data?.optedOutStats.optedOut} </Typography>
+                                
+
+                            </div>
+                        }
+                        <div  style={textDivStyle}>
+                            <Typography variant="subtitle2" > Total Opted Out </Typography>
+                            <Typography  variant="h5"> {data?.optedOutStats.optedOut} </Typography>
+
+                            
+                        </div>
+                    </div>
+                </div>
             }
         />
     )
